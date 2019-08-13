08c6395c21bdbd8d7abb32b019d55d727eb8e983
abirmajumder <abirmajumder@gmail.com>
2019-08-13 07:31:48 +0530

    first commit

---
+++ batchprocess/src/main/java/com/batchprocess/BatchprocessApplication.java
@@ -0,0 +1,17 @@
+package com.batchprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchprocessApplication  { // implements CommandLineRunner
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BatchprocessApplication.class, args);
+	}
+	
+	/*@Override
+    public void run(String... args) throws Exception { */
+        
+
+}
---
+++ batchprocess/src/main/java/com/batchprocess/StarterJob.java
@@ -0,0 +1,35 @@
+package com.batchprocess;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StarterJob {
+	
+	@Autowired
+    JobLauncher jobLauncher;
+     
+    @Autowired
+    Job job;
+	
+	public void perform() {
+		JobParameters params = new JobParametersBuilder()
+        							.addString("JobID", String.valueOf(System.currentTimeMillis()))
+        							.toJobParameters();
+		try {
+			jobLauncher.run(job, params);
+		} catch (JobExecutionAlreadyRunningException | JobRestartException
+				| JobInstanceAlreadyCompleteException
+				| JobParametersInvalidException e) {
+			e.printStackTrace();
+		}
+	}
+}
